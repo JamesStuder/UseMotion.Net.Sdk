f645021445fa858e1e8bac620c8604c334d439ac
James Studer <james@studer>
2023-05-08 21:31:31 -0400

    Init test for service.  Not run yet, just setting up each one.  Still need to add asserts then run them to test the project.

--- UseMotion.Net.Sdk/UseMotion.Net.Sdk.Tests/ApiServiceTests.cs
+++ UseMotion.Net.Sdk/UseMotion.Net.Sdk.Tests/ApiServiceTests.cs
@@ -1,13 +1,209 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 using NUnit.Framework;
-using UseMotion.Net.Sdk.Tests.Bases;
+using UseMotion.Net.Sdk.Interfaces;
+using UseMotion.Net.Sdk.Models;
+using UseMotion.Net.Sdk.Services;
 
-namespace UseMotion.Net.Sdk.Tests;
-
-public class ApiServiceTests : TestBase
+namespace UseMotion.Net.Sdk.Tests
 {
-    [Test]
-    public void Test1()
+    public class ApiServiceTests
     {
-        Assert.Pass();
+        private IMotionApi MotionApi { get; }
+        public ApiServiceTests()
+        {
+            MotionApi = new MotionService(Environment.GetEnvironmentVariable("MotionApiKey", EnvironmentVariableTarget.User)!);
+        }
+        
+        #region Tasks
+        [Test]
+        public void UpdateTaskTest()
+        {
+            const string taskId = "";
+            TaskPatch data = new()
+            {
+                Name = ""
+            };
+    
+            Task responseObject = MotionApi.UpdateTask(taskId, data);
+        }
+
+        [Test]
+        public void RetrieveTaskTest()
+        {
+            const string taskId = "";
+            Task responseObject = MotionApi.RetrieveTask(taskId);
+           
+        }
+
+        [Test]
+        public void DeleteTaskTest()
+        {
+            const string taskId = "";
+            bool responseObject = MotionApi.DeleteTask(taskId);
+        }
+
+        [Test]
+        public void CreateTaskTest()
+        {
+            TaskPost data = new ()
+            {
+                Name = "",
+                WorkspaceId = ""
+            };
+            Task responseObject = MotionApi.CreateTask(data);
+        }
+
+        [Test]
+        public void ListTasksTest()
+        {
+            const string workspaceId = "";
+            const string assigneeId = "";
+            const string cursor = "";
+            const string label = "";
+            const string name = "";
+            const string projectId = "";
+            const string status = "";
+            
+            ListTasks responseObject = MotionApi.ListTasks(workspaceId, assigneeId, cursor, label, name, projectId, status);
+        }
+
+        [Test]
+        public void MoveWorkspaceTest()
+        {
+            const string taskId = "";
+            MoveTask data = new ()
+            {
+                WorkspaceId = ""
+            };
+            Task responseObject = MotionApi.MoveWorkspace(taskId, data);
+        }
+        #endregion
+
+        #region Recurring Tasks
+        [Test]
+        public void CreateRecurringTaskTest()
+        {
+            RecurringTaskPost data = new ()
+            {
+                Frequency = "",
+                Name = "",
+                WorkspaceId = ""
+            };
+            string jsonToSend = JsonConvert.SerializeObject(data);
+            RecurringTask responseObject = MotionApi.CreateRecurringTask(data);
+        }
+
+        [Test]
+        public void ListRecurringTasksTest()
+        {
+            const string workspaceId = "";
+            const string cursor = "";
+
+            ListRecurringTasks responseObject = MotionApi.ListRecurringTasks(workspaceId, cursor);
+        }
+
+        [Test]
+        public void DeleteRecurringTaskTest()
+        {
+            const string id = "";
+            bool responseObject = MotionApi.DeleteRecurringTask(id);
+        }
+        #endregion
+
+        #region Comments
+        [Test]
+        public void CreateCommentTest()
+        {
+            CommentPost data = new ()
+            {
+                TaskId = "",
+                Content = ""
+            };
+
+            Comment responseObject = MotionApi.CreateComment(data);
+        }
+
+        [Test]
+        public void ListCommentsTest()
+        {
+            const string taskId = "";
+            const string cursor = "";
+
+            ListComments responseObject = MotionApi.ListComments(taskId, cursor);
+        }
+        #endregion
+
+        #region Projects
+        [Test]
+        public void RetrieveProject()
+        {
+            const string id = "";
+            Project responseObject = MotionApi.RetrieveProject(id);
+        }
+
+        [Test]
+        public void ListProjects()
+        {
+            const string workspaceId = "";
+            const string cursor = "";
+        
+            ListProjects responseObject = MotionApi.ListProjects(workspaceId, cursor);
+        }
+
+        [Test]
+        public void CreateProject()
+        {
+            ProjectPost data = new ()
+            {
+                Name = "",
+                WorkspaceId = ""
+            };
+            Project responseObject = MotionApi.CreateProject(data);
+        }
+        #endregion
+    
+        #region Workspaces
+        [Test]
+        public void ListStatusesForAWorkspaceTest()
+        {
+            const string workspaceId = "";
+
+            List<Status> responseObject = MotionApi.ListStatusesForAWorkspace(workspaceId);
+
+        }
+
+        [Test]
+        public void ListWorkspacesTest()
+        {
+            const string cursor = "";
+            const string[]? ids = null;
+
+            ListWorkspaces responseObject = MotionApi.ListWorkspaces(cursor, ids);
+
+        }
+        #endregion
+    
+        #region Users
+        [Test]
+        public void ListUsers()
+        {
+            const string cursor = "";
+            const string teamId = "";
+            const string workspaceId = "";
+
+            ListUsers responseObject = MotionApi.ListUsers(cursor, teamId, workspaceId);
+        }
+        #endregion
+    
+        #region Schedules
+        [Test]
+        public void GetSchedules()
+        {
+            List<Schedule> responseObject = MotionApi.GetSchedules();
+        }
+        #endregion
     }
 }
